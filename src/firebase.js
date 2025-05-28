21eb4de74b37c402a19f3e873f987af126541bec
huohu125 <96034111+huohu125@users.noreply.github.com>
2025-05-28 20:06:43 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,24 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// 从 Firebase 控制台获取的配置信息
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB26UN0xUbXuNwT92c24rpqdQgTb6mgZT0",
   authDomain: "cangkuguanli-57be3.firebaseapp.com",
-  databaseURL: "https://cangkuguanli-57be3.firebaseio.com",
+  databaseURL: "https://cangkuguanli-57be3-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "cangkuguanli-57be3",
   storageBucket: "cangkuguanli-57be3.firebasestorage.app",
   messagingSenderId: "680909943577",
-  appId: "1:680909943577:web:60d83cee9f59f229a10b88",
-  measurementId: "G-VW9M3F87XQ"
+  appId: "1:680909943577:web:7c01629353fb37a6a10b88",
+  measurementId: "G-R73KJ7BDDD"
 };
 
-// 初始化 Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// 获取数据库实例并导出
-export const database = getDatabase(app);
+export { app, analytics };
