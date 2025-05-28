5f855cb55c754be7f00a662b0ad8d92196e926b6
huohu125 <96034111+huohu125@users.noreply.github.com>
2025-05-28 22:37:37 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// src/firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyB26UN0xUbXuNwT92c24rpqdQgTb6mgZT0",
   authDomain: "cangkuguanli-57be3.firebaseapp.com",
@@ -13,12 +6,9 @@ const firebaseConfig = {
   projectId: "cangkuguanli-57be3",
   storageBucket: "cangkuguanli-57be3.firebasestorage.app",
   messagingSenderId: "680909943577",
-  appId: "1:680909943577:web:7c01629353fb37a6a10b88",
-  measurementId: "G-R73KJ7BDDD"
+  appId: "1:680909943577:web:7c01629353fb37a6a10b88"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export { app, analytics };
+// 初始化Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
